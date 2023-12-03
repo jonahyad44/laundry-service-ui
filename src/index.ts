ccc7e66465573ca6927aedf452fc7b2610b34390
jonahyad44 <jonahya.daniel@gmail.com>
2023-12-03 17:15:28 -0500

    fixed port

--- src/index.ts
+++ src/index.ts
@@ -1,138 +1,156 @@
-import express, { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import cors from 'cors';
-import { json } from 'body-parser';
-import Stripe from 'stripe';
-import { env } from "node:process";
 import { PrismaClient } from "@prisma/client";
+import express, { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import bodyParser from 'body-parser';
+import axios from "axios";
+const stripe = require('stripe')('sk_test_51ODvYwDEtVoGXwUaSYKqzFhtt9WYjxTh3D3kJtxMGkWvZGeEni0VDmgAXkiG28VRCcMlqEnCRwB9CqcgbDE2bURW00JW1Wkp5g');
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const bodyParser = require('body-parser');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const port = 3000;
 
 const prisma = new PrismaClient();
 const app = express();
-const stripe = new Stripe('sk_test_51ODvYwDEtVoGXwUaSYKqzFhtt9WYjxTh3D3kJtxMGkWvZGeEni0VDmgAXkiG28VRCcMlqEnCRwB9CqcgbDE2bURW00JW1Wkp5g', {
-    apiVersion: '2023-10-16', // Use the latest API version
-  });
-  
+const STRIPE_SECRET_KEY = 'sk_test_51ODvYwDEtVoGXwUaSYKqzFhtt9WYjxTh3D3kJtxMGkWvZGeEni0VDmgAXkiG28VRCcMlqEnCRwB9CqcgbDE2bURW00JW1Wkp5g';
 
-app.use(json());
 app.use(express.json());
-app.use(express.static('public'));
-app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(cors());
 
-app.post("/api/register", async (req, res) => {
+interface AuthenticatedRequest extends Request {
+  user: { username: string; password: string }; 
+}
+
+app.post("/charge", async (req, res) => {
   try {
-    const username = req.body.username;
-    console.log("Req Recieved");
-    const hashedPass = await bcrypt.hash(req.body.password, 10);
-    console.log("Pass hashed");
-    await prisma.admin.create({
-      data: {
-        username: username,
-        password: hashedPass,
-      },
-    });
-    res.status(201).send();
-  } catch {
-    res.status(500).send();
-  }
-  });
-
-  app.post("/api/login", async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-  const user = await prisma.admin.findFirst({
-    where: { username: username },
-  });
-  if (!user) {
-    return res.status(400).send("User not found");
-  }
-
-  const correct = await bcrypt.compare(password, user.password);
-  if (!correct) {
-    res.sendStatus(403);
-  } else {
-    const token: string = jwt.sign(username, env.JWT_SECRET_KEY);
-    res.status(201).json(token);
-  }
-  });
-
-  app.post("/api/checkToken", (req: Request, res: Response) => {
-    const token = req.body.token;
-    if (token) {
-      jwt.verify(token, env.JWT_SECRET_KEY, (err: Error, user: string) => {
-        if (err) {
-          res.sendStatus(403);
-        } else {
-          res.status(200).json(user);
-        }
-      });
-    } else {
-      res.sendStatus(401);
-    }
-  });
-
-      app.post("/api/submitorder", async (req: Request, res: Response) => {
-        try {   
-          const formData = req.body.formValues;
-          console.log(formData);
-      res.status(200).json({ message: "Form submission successful" });
-    } catch (error) {
-      console.error("Error during form submission:", error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  });
-
-      app.post("/api/contactform", async (req, res) => {
-        const {name, email, message} = req.body;
-        if(!name) {
-          return res.status(400).send("name required");
-        }
-        if(!email) {
-          return res.status(400).send('email required');
-        }
-      
-        if(!message) {
-          return res.status(400).send('message required');
-        }
-        else {
-          res.status(200).send("Email sent");
-        }
-      
-       });
-
-
-       app.get("/api/orders", async (req, res) => {
-        try {
-          const orders = await stripe.issuing.transactions.list();
-          res.json(orders.data);
-          console.log(req.body);
-        } catch (error) {
-          console.error("Error retriveing orders:", error);
-          res.status(500).json({ error: 'Server error' });
-        }
-      });
-
-      module.exports = function(app) {
-        app.use(
-          "/api",
-          createProxyMiddleware({
-            target: "http://localhost:8080",
-            changeOrigin: true,
-          })
-        );
-      };
-         
-
-       
+      const { amount, token } = req.body;
+      const charge = await stripe.charges.create({
+          amount,
+          currency: 'usd',
+          source: token,
+        });
     
-app.listen(8080, () => {
-    console.log("Server ready at http://localhost:8080");
+        // Handle successful payment (e.g., update database, send confirmation email)
+        // Send a success response to the client
+        res.status(200).json({ success: true });
+      } catch (error) {
+        console.error(error.message);
+        // Handle errors and send a failure response to the client
+        res.status(500).json({ error: 'Payment failed' });
+      }
+    });
+
+    app.get('/api/orders', async (req, res) => {
+      try {
+        const response = await axios.get('https://api.stripe.com/v1/orders', {
+          headers: {
+            Authorization: `Bearer ${STRIPE_SECRET_KEY}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
+    
+        const orders = response.data.data;
+        res.json({ orders });
+      } catch (error) {
+        console.error('Error retrieving orders:', error.response.data.error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+
+
+app.post("/signup", async (req, res) => {
+    try {
+      const username = req.body.username;
+      console.log("Username Recieved");
+      const hashedPass = await bcrypt.hash(req.body.password, 10);
+      console.log("Password created");
+      await prisma.admin.create({
+        data: {
+          username: username,
+          password: hashedPass,
+        },
+      });
+      res.status(201).send();
+    } catch {
+      res.status(500).send();
+    }
+  });
+
+  app.post("/checkToken", (req, res) => {
+    const { token } = req.body;
+    if (token === 'valid_token') {
+      res.status(200).json({ valid: true, message: 'Token is valid' });
+    } else {
+      res.status(401).json({ valid: false, message: 'Token is invalid' });
+    }
+  });
+
+
+app.post("/contactform", async (req, res) => {
+  const {name, email, message} = req.body;
+  if(!name) {
+    return res.status(400).send("name required");
+  }
+  if(!email) {
+    return res.status(400).send('email required');
+  }
+
+  if(!message) {
+    return res.status(400).send('message required');
+  }
+  else {
+    res.status(200).send("Email sent");
+  }
+
+ });
+
+
+
+
+  app.post("/login", async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    const user = await prisma.admin.findFirst({
+      where: { username: username },
+    });
+    if (!user) {
+      return res.status(400).send("Cannot find user");
+    }
+    if (await bcrypt.compare(user.password, password)) {
+      res.status(403).send("Incorrect Password");
+    } else {
+      const token: string = jwt.sign(user, process.env.JWT_KEY, {
+        expiresIn: "1h",
+      });
+      res.status(201).send();
+    }
+  });
+
+  function authenticateToken(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization?.split(" ")[1] || "";
+  
+    jwt.verify(
+      token,
+      process.env.JWT_KEY,
+      (err: Error, user: { username: string; password: string }) => {
+        if (err) {
+          return res.sendStatus(401);
+        }
+  
+        (req as AuthenticatedRequest).user = user as {
+          username: string;
+          password: string;
+        }; 
+  
+        next();
+      }
+    );
+  }
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
 
 module.exports = app;
-
